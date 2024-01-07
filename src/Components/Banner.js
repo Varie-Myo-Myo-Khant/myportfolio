4865e6d3e7b7501d4e325c777754b9b989724898
Varie <myokhant122021@gmail.com>
2024-01-07 21:28:24 +0630

    nav updates

---
+++ src/Components/Banner.js
@@ -0,0 +1,17 @@
+import { Container,Row ,Col} from "react-bootstrap"
+export const Banner =()=>{
+    return(
+        <section className="banner" id="home">
+            <Container>
+                <Row>
+                    <Col>
+                    <span className="tagline">Welcome to my Portfolio</span>
+                    <h1>{`Hi I'm Embedded`}<span>Web Developer</span></h1>
+                    <p>dkdkdk dkkkk dkkkkk dkkkkkkkkk</p>
+                    <button onClick={}></button>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
